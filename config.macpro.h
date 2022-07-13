ff01a94b1be269d4e39e7504d342026a8cf7312b
Gerardo Marx <gmarx_cc@itmorelia.edu.mx>
2022-07-12 20:51:27 -0500

    last macpro configuration with dwm

--- config.macpro.h
+++ config.macpro.h
@@ -41,7 +41,7 @@ static const unsigned int alphas[][3]      = {
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "" ,"", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -50,7 +50,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Brave",    NULL,       NULL,       0<<1,         0,           -1 },
+	//{ "Brave",    NULL,       NULL,       0<<1,         0,           -1 },
 	{ "imagej",   NULL,       NULL,       0,            1,           -1 },
 	{ "eclipse",  NULL,       NULL,       1<<4,  	    1,           -1 },
 	{ "Arduino",  NULL,       NULL,       1<<4, 	    1,           -1 },
